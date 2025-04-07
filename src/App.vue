47bbbec8ee2f05b86863c0ad2de3c3f5429fe231
Icedevil777 <icedevil777228@mail.ru>
2025-04-07 21:18:58 +0300

    refactor(decomposition into components): create OpenProductDialog
    #1

--- src/App.vue
+++ src/App.vue
@@ -29,16 +29,7 @@
 import { Button } from '@/components/ui/button';
 import { X, ShoppingCart } from 'lucide-vue-next';
 import { Card, CardHeader, CardTitle } from '@/components/ui/card';
-
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
+import { OpenProductDialog } from '@/components/ui/dialog';
 
 import {
   NumberField,
@@ -49,27 +40,15 @@ import {
 } from '@/components/ui/number-field';
 
 import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import {
   getProductControllerFindAllQueryKey,
   useProductControllerFindAll,
   useProductControllerRemove,
 } from './api/products/products.ts';
 
-import { Gap } from '@/components/ui/Gap';
-import { Textarea } from '@/components/ui/textarea';
-import { toTypedSchema } from '@vee-validate/zod';
-import { useForm } from 'vee-validate';
-import { watch, ref } from 'vue';
+import { watch } from 'vue';
 
 import { Toaster } from 'vue-sonner';
-import * as z from 'zod';
+
 import { useQueryClient } from '@tanstack/vue-query';
 import type { CartResponseDto, ProductListResponseDto } from '@/api/model';
 import {
@@ -81,39 +60,10 @@ import {
 } from '@/api/cart/cart.ts';
 import { Badge } from '@/components/ui/badge';
 import { APP_CONFIG } from '@/config';
-import { useCreateProductWithProviders } from './infrostruct/service';
 
 const USER_ID = APP_CONFIG.USER_ID;
 const { data: products } = useProductControllerFindAll();
 const queryClient = useQueryClient();
-
-const { create: createProduct, response: createProductResponse } =
-  useCreateProductWithProviders();
-
-watch(createProductResponse, () => {
-  isCreateModalOpen.value = false;
-});
-
-const formSchema = toTypedSchema(
-  z.object({
-    name: z.string().min(2).max(50),
-    description: z.string().max(2000),
-    price: z.number().positive(),
-    imageUrl: z.string(),
-    stock: z.number(),
-    category: z.string(),
-  }),
-);
-
-const { isFieldDirty, handleSubmit } = useForm({
-  validationSchema: formSchema,
-});
-
-const onSubmit = handleSubmit((data) => {
-  createProduct({
-    data,
-  });
-});
 
 const { mutate: deleteProduct, data: removeResponse } =
   useProductControllerRemove();
@@ -124,8 +74,6 @@ const handleDeleteProduct = (productId: number) => {
 };
 
 const { data: cartData } = useCartControllerGetCart({ userId: USER_ID });
-
-const isCreateModalOpen = ref(false);
 
 watch(removeResponse, () => {
   invalidateProductFindAll();
@@ -220,122 +168,7 @@ function invalidateCart() {
   <div class="p-4">
     <div class="flex items-center gap-2 mb-4">
       <h1>Товары</h1>
-
-      <Dialog v-model:open="isCreateModalOpen">
-        <DialogTrigger as-child>
-          <Button variant="outline"> Добавить товар </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Добавление товара</DialogTitle>
-            <DialogDescription>
-              Введите данные для добавления товара и сохраните результат
-            </DialogDescription>
-          </DialogHeader>
-          <form class="max-h-[70vh] overflow-scroll" @submit="onSubmit">
-            <FormField
-              v-slot="{ componentField }"
-              name="name"
-              :validate-on-blur="!isFieldDirty"
-            >
-              <FormItem v-auto-animate>
-                <FormLabel>Название товара</FormLabel>
-                <FormControl>
-                  <Input type="text" v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-
-            <Gap size="sm" />
-
-            <FormField
-              v-slot="{ componentField }"
-              name="description"
-              :validate-on-blur="!isFieldDirty"
-            >
-              <FormItem v-auto-animate>
-                <FormLabel>Описание товара</FormLabel>
-                <FormControl>
-                  <Textarea v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-
-            <Gap size="sm" />
-
-            <FormField
-              v-slot="{ componentField }"
-              name="price"
-              :validate-on-blur="!isFieldDirty"
-            >
-              <FormItem v-auto-animate>
-                <FormLabel>Цена</FormLabel>
-                <FormControl>
-                  <Input type="number" v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-
-            <Gap size="sm" />
-
-            <FormField
-              v-slot="{ componentField }"
-              name="imageUrl"
-              :validate-on-blur="!isFieldDirty"
-            >
-              <FormItem v-auto-animate>
-                <FormLabel>Изображение</FormLabel>
-                <FormControl>
-                  <Input type="text" v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-
-            <Gap size="sm" />
-
-            <FormField
-              v-slot="{ componentField }"
-              name="stock"
-              :validate-on-blur="!isFieldDirty"
-            >
-              <FormItem v-auto-animate>
-                <FormLabel>Осталось товаров</FormLabel>
-                <FormControl>
-                  <Input type="number" v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-
-            <Gap size="sm" />
-
-            <FormField
-              v-slot="{ componentField }"
-              name="category"
-              :validate-on-blur="!isFieldDirty"
-            >
-              <FormItem v-auto-animate>
-                <FormLabel>Категория</FormLabel>
-                <FormControl>
-                  <Input type="text" v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-
-            <Gap size="sm" />
-
-            <DialogFooter>
-              <Button class="w-full" type="submit"> Добавить </Button>
-            </DialogFooter>
-          </form>
-        </DialogContent>
-      </Dialog>
-
+      <OpenProductDialog />
       <Badge v-if="cartData" class="h-6 rounded-2xl">
         <ShoppingCart />
         {{ cartData.items?.reduce((acc, item) => (acc += item.quantity), 0) }}
