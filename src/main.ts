a644dd15afd58e9cfa2a4f4dc6eff93b15cbb281
Icedevil777 <icedevil777228@mail.ru>
2025-04-07 22:47:31 +0300

    fix(fix by lint): fix ;
    #1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 
 const app = createApp(App);
